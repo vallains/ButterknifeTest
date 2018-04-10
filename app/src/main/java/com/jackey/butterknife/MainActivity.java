adb0b9a1df0f0b32811e0628e03a7702576ef837
wangpu1993 <623327366@qq.com>
2018-04-10 11:19:47 +0800

    --

--- app/src/main/java/com/jackey/butterknife/MainActivity.java
+++ app/src/main/java/com/jackey/butterknife/MainActivity.java
@@ -1,12 +1,19 @@
 package com.jackey.butterknife;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.BindArray;
+import butterknife.BindBitmap;
+import butterknife.BindColor;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +23,25 @@ public class MainActivity extends Activity {
     @BindString(R.string.app_name)
     public String appName;
 
+    @BindArray(R.array.citys)
+    public String[] citys;
+
+    @BindBitmap(R.mipmap.ic_launcher)
+    public Bitmap a;
+
+    @BindColor(R.color.colorAccent)
+    public int black;
+
+    @OnClick(R.id.textView)
+    public void showToast(){
+        Toast.makeText(this, "is a click", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnLongClick(R.id.textView)
+    public boolean showLongToast(){
+        Toast.makeText(this, "is a longclick", Toast.LENGTH_SHORT).show();
+        return true;
+    }
 
 
     @Override
@@ -24,6 +50,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        textView.setText(appName);
+        textView.setText(citys[1]);
     }
 }
